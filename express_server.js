526df571301ba9a95f5dc093a3eb36f2f1e689d1
Saeed <saeed6433@gmail.com>
2023-03-16 15:24:05 -0400

    form - get request

--- express_server.js
+++ express_server.js
@@ -26,10 +26,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase.id };
   res.render("urls_show", templateVars);
 });
+
 
 
 app.listen(PORT, () => {
