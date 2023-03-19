ec146a39307ff7a1c756801e3fa0f6259ce31198
Saeed <saeed6433@gmail.com>
2023-03-19 14:35:57 -0400

    user registration form

--- express_server.js
+++ express_server.js
@@ -79,6 +79,11 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = {username: req.cookies.username };
+  res.render("register_view", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
